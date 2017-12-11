e3bcdebdd2ba9b8707e065497ee9a2b222ea636d
chrisomar1 <chrisomar_093@hotmail.com>
2017-12-11 12:08:12 -0600

    Se agrega el proyecto creado en netbeans

---
+++ src/pilasycolas/PilasYColas.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pilasycolas;
+import java.io.*;
+import java.util.*;
+/**
+ *
+ * @author ChristianOmar
+ */
+public class PilasYColas {
+
+    LinkedList queue;
+    LinkedList stack;
+    
+    PilasYColas()
+    {
+        queue = new LinkedList();
+        stack = new LinkedList();
+    
+    }
+    
+    public void pushCharacter(char x)
+    {
+        stack.addFirst(x);
+    }
+    
+    public void enqueueCharacter(char y)
+    {
+        queue.addLast(y);
+    }
+    
+    public char popCharacter()
+    {
+        return (char) stack.pop();
+    }
+    
+    public char dequeueCharacter()
+    {
+        return (char) queue.removeFirst();
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        scan.close();
+
+        // Convert input String to an array of characters:
+        char[] s = input.toCharArray();
+
+        // Create a Solution object:
+        PilasYColas p = new PilasYColas();
+
+        // Enqueue/Push all chars to their respective data structures:
+        for (char c : s) {
+            p.pushCharacter(c);
+            p.enqueueCharacter(c);
+        }
+
+        // Pop/Dequeue the chars at the head of both data structures and compare them:
+        boolean isPalindrome = true;
+        for (int i = 0; i < s.length/2; i++) {
+            if (p.popCharacter() != p.dequeueCharacter()) {
+                isPalindrome = false;                
+                break;
+            }
+        }
+
+        //Finally, print whether string s is palindrome or not.
+        System.out.println( "The word, " + input + ", is " 
+                           + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
+    }
+    
+}
